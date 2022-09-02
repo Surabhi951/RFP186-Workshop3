6a3927f53d8b0f23ef0bde11a35197d99a0bc96b
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-02 20:30:12 +0530

    Added: initial game setup with deck of cards

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -7,22 +7,21 @@ public class DeckOfCards {
     }
 
     void initializeCards(){
-        System.out.println("initializing cards");
-        int cardIndex = 0;
-        for(int i = 0; i < Card.suitArray.length; i++){
-            for(int j = 0; j < Card.rankArray.length; j++){
-                Card card = new Card(Card.suitArray[i],Card.rankArray[j]);
-                Card.cardArray[cardIndex++] = card;
+        System.out.println("initializing cards...");
+        int cardIndex=0;
+        for (int i = 0; i < Card.suitArray.length; i++) {
+            for (int j = 0; j < Card.rankArray.length; j++) {
+                Card card=new Card(Card.suitArray[i],Card.rankArray[j]);
+                Card.cardArray[cardIndex++]=card;
             }
         }
         printCards(Card.cardArray);
     }
-
-
     void printCards(Card[] cards){
-        System.out.println("Cards are:");
-        for(Card card : cards){
-            System.out.println(card.getSuit() + "-" + card.getRank() + ",");
+        System.out.print("Cards{");
+        for (Card card : cards) {
+            System.out.print(card.getSuit() + "-" + card.getRank() + ", ");
         }
+        System.out.println("}");
     }
 }
