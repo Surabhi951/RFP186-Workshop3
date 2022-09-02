cf70c64e028d0383881efd6ca8692a60365249cb
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-02 21:34:50 +0530

    added no of players

--- src/com/bridgelabz/Player.java
+++ src/com/bridgelabz/Player.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Player {
     Card[] playerCards =new Card[9];
     static ArrayList<Player> playerList=new ArrayList<>();
-
     public static void addPlayer(Player player){
         playerList.add(player);
     }
